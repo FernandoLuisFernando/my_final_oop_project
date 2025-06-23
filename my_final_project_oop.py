17c0a7f3365a2e5881f858a2659bf0113cc0d668
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-06-23 14:37:51 +0800

    screen_settings

--- my_final_project_oop.py
+++ my_final_project_oop.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import random
 
-GAME_WIDTH = 700
-GAME_HEIGHT = 700
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3 
@@ -14,7 +14,8 @@ class Snake:
     pass 
 
 class Food:
-    pass
+    
+    def
 
 def next_turn():
     pass
@@ -27,3 +28,31 @@ def check_collisions():
 
 def game_over():
     pass
+
+window = Tk()
+window.title("snake game")
+window.resizable(False, False)
+
+score = 0 
+direction = 'down'
+
+label = Label(window, text='score:{}'.format(score), font=('consolas, 40'))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenwidth()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+ 
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+window.mainloop()
