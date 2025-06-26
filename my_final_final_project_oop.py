0dd26673afaf30ce73fdbc627d4e2dd0a996e033
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-06-27 04:23:25 +0800

    apple_image

--- my_final_final_project_oop.py
+++ my_final_final_project_oop.py
@@ -31,13 +31,12 @@ class SNAKE:
 
 class FRUIT:
     def __init__(self):
-        self.x = random.randint(0,cell_number - 1)
-        self.y = random.randint(0,cell_number - 1)
-        self.pos = Vector2(self.x,self.y)
+        self.randomize()
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size,cell_size)
-        pygame.draw.rect(screen,(126, 166, 114), fruit_rect)
+        screen.blit(apple,fruit_rect)
+        #pygame.draw.rect(screen,(126, 166, 114), fruit_rect)
 
     def randomize(self ):
         self.x = random.randint(0,cell_number - 1)
